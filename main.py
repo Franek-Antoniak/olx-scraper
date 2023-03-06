9b2fcf072dc33ee191862349cc0160ea942c11be
Franek Antoniak <franekant123@gmail.com>
2023-03-06 12:11:12 +0100

    scraper with selenium

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # import bibliotek
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import webbrowser
 
 # ustawienie adresu strony
 url = input("Enter OLX page url: ")
-prefix = 'https://www.olx.pl'
 max_price = int(input("Enter max price: "))
-# pobranie strony za pomocą requests
-page = requests.get(url)
-
-# utworzenie obiektu BeautifulSoup
-soup = BeautifulSoup(page.content, 'html.parser')
+# otworzenie przeglądarki i przejście do strony z ofertami
+browser = webdriver.Chrome()
+browser.get(url)
 
 
 class Offer:
@@ -25,24 +26,26 @@ class Offer:
 
 # lista linków do ofert
 links = []
-
 page_number = 1
-
 # pętla przechodząca przez wszystkie strony z ofertami
 while True:
 
     # pobranie wszystkich ofert na danej stronie
-    offers = soup.find_all('div', attrs={'data-cy': 'l-card'})
+    offers = WebDriverWait(browser, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-cy="l-card"]')))
 
     # dla każdej oferty pobierz link i cenę
     for offer in offers:
 
         # link do oferty
-        link = offer.find('a', class_='css-rc5s2u')['href']
-        if link.startswith('https://'):
+        link = offer.find_element(By.CSS_SELECTOR, 'a[class="css-rc5s2u"]').get_attribute('href')
+
+        if not link.startswith('https://www.olx.pl/'):
             continue
+
         # cena oferty
-        offer_page = requests.get(prefix + link)
+        # otwórz nową kartę i przejdź do oferty
+        offer_page = requests.get(link)
         offer_soup = BeautifulSoup(offer_page.content, 'html.parser')
         price = offer_soup.find('h3', class_='css-ddweki er34gjf0').text
         price = int(price.replace(' zł', '').replace(' ', ''))
@@ -53,6 +56,7 @@ while True:
         for rent_paragraph in rent_paragraphs:
             if rent_paragraph.text.startswith('Czynsz (dodatkowo): '):
                 rent = rent_paragraph.text
+                break
         rent = float(rent.replace('Czynsz (dodatkowo): ', '').replace(' zł', '').replace(' ', '').replace(',', '.'))
 
         # całkowita cena oferty
@@ -60,19 +64,22 @@ while True:
 
         # jeżeli całkowita cena oferty nie przekracza 3200 zł to dodaj link do listy linków
         if total_price <= max_price:
-            offer = Offer(prefix + link, total_price)
+            offer = Offer(link, total_price)
             links.append(offer)
+            print(offer)
+
     # sprawdź czy jest następna strona
     page_name = 'Page ' + str(page_number + 1)
     page_number += 1
-    next_page = soup.find('a', attrs={'data-cy': "pagination-forward"})
-    if next_page is None or page_number > 14:
+    next_page = browser.find_element(By.CSS_SELECTOR, 'a[data-cy="pagination-forward"]')
+
+    if next_page is None:
         break
+
     print("Going to next page " + page_name)
+
     # jeżeli jest następna strona to pobierz ją i przejdź do następnej iteracji pętli
-    next_page_url = prefix + next_page['href']
-    page = requests.get(next_page_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    next_page.click()
 
 # sort list of offers by price
 links.sort(key=lambda x: x.full_price)
@@ -84,5 +91,6 @@ while True:
     from_index = int(input())
     print("To: ")
     to_index = int(input())
+
     for i in range(from_index, to_index):
         webbrowser.open_new_tab(links[i].link)
